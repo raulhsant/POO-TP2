2a16ec913ffae66c8cdc0c567580a9831177f009
Raul Santana <raulhsantana@gmail.com>
2018-06-25 22:57:23 -0300

    Created project and class Ligacao

---
+++ src/operadora/Ligacao.java
@@ -0,0 +1,48 @@
+package operadora;
+
+import java.util.Date;
+
+public class Ligacao {
+	
+	private Date data;	
+	private Integer duracao; //em minutos
+	private double valor;
+//	private Integer destinatario;
+	
+	
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+	public Integer getDuracao() {
+		return duracao;
+	}
+	public void setDuracao(Integer duracao) {
+		this.duracao = duracao;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+//	public Integer getDestinatario() {
+//		return destinatario;
+//	}
+//	public void setDestinatario(Integer destinatario) {
+//		this.destinatario = destinatario;
+//	}
+//	
+	public Ligacao(Date data, Integer duracao, double valor) {
+		super();
+		this.data = data;
+		this.duracao = duracao;
+		this.valor = valor;
+	}
+	public Ligacao() {
+		super();
+	}
+
+}
